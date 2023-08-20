1c188b9066ce9e682d0abf50fcb96e4e0dff47c3
Alex Levchuk <alexlevchuk@mail.ru>
2023-08-20 19:40:09 +0300

    use PropTypes for types check

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -114,6 +115,10 @@ const View = ({ character }) => {
 			</ul>
 		</>
 	);
+};
+
+CharInfo.propTypes = {
+	characterId: PropTypes.number,
 };
 
 export default CharInfo;
